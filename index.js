c252d6a72873e6f244b0be46fb65d3d2076f0d32
dydy5269-hash <dydy5269@gmail.com>
2026-03-27 04:40:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -53,12 +53,11 @@ app.post("/webhook", async (req, res) => {
     const tech = data.tech;
     const location = data.location;
 
-    if (text.trim() === "1") {
-      let mapLink = "لا يوجد موقع";
+    let mapLink = "لا يوجد موقع";
 
-  if (location && location.latitude && location.longitude) {
-    mapLink = https://maps.google.com/?q=${location.latitude},${location.longitude};
-  }
+if (location && location.latitude && location.longitude) {
+  mapLink = `https://maps.google.com/?q=${location.latitude},${location.longitude}`;
+}
       await sendMessage(
         client,
         `🚀 تم تأكيد طلبك بنجاح
