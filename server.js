7e7166c6da40348d187849232c069db97967c52f
dydy5269-hash <dydy5269@gmail.com>
2026-03-28 06:58:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-cc
+import express from "express";
+import path from "path";
+
+const app = express();
+app.use(express.json());
+
+// عرض ملفات public
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
+});
