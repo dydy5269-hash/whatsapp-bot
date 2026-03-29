fdd415dba97050d17df6b149386b7d7f6229ec19
dydy5269-hash <dydy5269@gmail.com>
2026-03-29 05:43:15 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require "express;
+const express = require "express";
 const axios = require “axios”;
 const admin = require “firebase-admin”;
 const { v4: uuidv4 } = require(“uuid");
